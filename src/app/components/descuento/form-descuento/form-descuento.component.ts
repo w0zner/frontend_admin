b4e985393a5779adeb416c160c78c59e3c7ca4c1
cesar_ramirez <cesar_ramirez@hacienda.gov.py>
2025-09-24 16:54:27 -0300

    Add form descuento

--- src/app/components/descuento/form-descuento/form-descuento.component.ts
+++ src/app/components/descuento/form-descuento/form-descuento.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { NotificacionService } from 'src/app/services/notificacion.service';
 
 @Component({
   selector: 'app-form-descuento',
   templateUrl: './form-descuento.component.html',
   styleUrls: ['./form-descuento.component.css']
 })
-export class FormDescuentoComponent {
+export class FormDescuentoComponent implements OnInit{
 
+  imgSelect: any | ArrayBuffer = 'assets/img/01.jpg'
+  file: File | undefined = undefined
+  productId: string | null = null
+  descuentoForm: FormGroup
+  id:any= null
+
+  constructor(private fb: FormBuilder, private route: ActivatedRoute, private notificacionService: NotificacionService) {
+    this.descuentoForm = this.fb.group({
+      titulo: [''],
+      descuento: [0],
+      fecha_inicio: [''],
+      fecha_fin: [''],
+      banner: ['']
+    })
+  }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      console.log(params.get('id'))
+      this.id = params.get('id')
+    })
+  }
+
+  registrar() {
+
+  }
+
+    fileChangeEvent(event:any): void {
+    let file;
+    if(event.target.files && event.target.files[0]) {
+      file = <File> event.target.files[0];
+      console.log(file)
+    } else {
+      this.notificacionService.notificarError(null, "No hay archivo de imagen")
+    }
+
+    if(file && file.size <= 4000000) {
+      if(file.type=='image/png' || file.type=='image/jpeg' || file.type=='image/jpg' || file.type=='image/gif') {
+        const reader = new FileReader()
+        reader.onload = e => this.imgSelect = reader.result;
+        console.log(this.imgSelect)
+
+        reader.readAsDataURL(file)
+        this.file = file
+      } else {
+        this.notificacionService.notificarError(null, "El archivo debe ser una imagen válida")
+        this.imgSelect= 'assets/img/01.jpg'
+        this.file = undefined
+      }
+    } else {
+      this.notificacionService.notificarError(null, "La imagen no puede ser mayor a 4MB")
+      this.imgSelect= 'assets/img/01.jpg'
+      this.file = undefined
+    }
+  }
 }
